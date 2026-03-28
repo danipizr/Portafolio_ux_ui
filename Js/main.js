3434cdf8dd10a7d210beb5a62f139bdac4a28350
Danipiz <danipizr55@gmail.com>
2026-03-27 21:46:59 -0300

    first commit

---
+++ Js/main.js
@@ -0,0 +1,47 @@
+const elements = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+});
+
+elements.forEach(el => observer.observe(el));
+
+
+// menu hamburguesa
+const toggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav-links");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+
+// cierre menu
+document.querySelectorAll(".nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+});
+
+window.addEventListener("scroll", () => {
+  const hero = document.querySelector(".hero");
+  const scrollY = window.scrollY;
+
+  hero.style.transform = `translateY(${scrollY * 0.2}px)`;
+});
+
+const process = document.querySelector(".process-list");
+
+const observerProcess = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      process.classList.add("visible");
+    }
+  });
+});
+
+observerProcess.observe(process);
